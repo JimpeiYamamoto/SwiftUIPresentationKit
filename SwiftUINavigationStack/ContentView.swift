10ae906f4fe6c318f8107617b9158699a5769087
JimpeiYamamoto <zinpei104@gmail.com>
2025-10-26 14:23:32 +0900

    Initial Commit

---
+++ SwiftUINavigationStack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUINavigationStack
+//
+//  Created by 山本 迅平 on 10/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUINavigationStack/SwiftUINavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationStackApp.swift
+//  SwiftUINavigationStack
+//
+//  Created by 山本 迅平 on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
